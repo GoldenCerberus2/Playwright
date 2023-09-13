63f98b3b5651eb359c110fe10afc1737122f0f81
GoldenCerberus <Goldencerberus@michalpol.pl>
2023-09-13 16:12:17 +0200

    Creating New test

---
+++ tests/LumaBuyingTest.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test, } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+
+
+// Going to the website
+    await page.goto('https://www.saucedemo.com/');
+
+
+
+});
